fcd536575ad18e770e470048c08a5021f8a454b7
Albina MV <alyamarvlad@gmail.com>
2022-03-14 14:28:28 +1000

    Reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
-    public String calculate (int mass, int height) {
+    public String calculate(int mass, int height) {
         double bodyMassIndex = mass / (height * height * 0.0001);
-        String result = String.format("%.1f",bodyMassIndex); // округление выводимого результата до 1 знака после запятой
+        String result = String.format("%.1f", bodyMassIndex); // округление выводимого результата до 1 знака после запятой
         return result;
     }
 }
